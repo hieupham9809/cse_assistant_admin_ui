692bd1c64a7e03719237e9a54f018e3d81b8bb74
hieu pham <hieupham9809@gmail.com>
2020-04-13 23:25:09 +0700

    replace api online

--- src/dragging_form_add_new_activity/constants.js
+++ src/dragging_form_add_new_activity/constants.js
@@ -1,12 +1,12 @@
 // export const PREDICT_API = 'https://real-estate-extraction.herokuapp.com/api';
 // export const PREDICT_API = 'https://ner-server-hieupm.herokuapp.com/api/tagging/v1';
 // export const PREDICT_API = ['https://ner-server-hieupm.herokuapp.com/api/tagging/v1','https://ner-deploy-hieupm.herokuapp.com/api/tagging/v1','https://ner-server-prediction.herokuapp.com/api/tagging/v1','https://server-ner-prediction-second.herokuapp.com/api/tagging/v1'];
-export const PREDICT_API = ['http://127.0.0.1:5000/api/tagging/v1'];
-export const LOGIN_API = ['http://127.0.0.1:5002/auth']
-export const INSERT_API = ['http://127.0.0.1:5002/api/server-cse-assistant-admin/activities']
-export const GET_DB_API = ['http://127.0.0.1:5002/api/server-cse-assistant-admin/activities/page/']
-export const GET_DB_FILTER = ['http://127.0.0.1:5002/api/server-cse-assistant-admin/activities/filter/page/']
-export const GET_SINGLE_API = ['http://127.0.0.1:5002/api/server-cse-assistant-admin/activities/']
-export const DELETE_SINGLE_API = ['http://127.0.0.1:5002/api/server-cse-assistant-admin/activities/']
-export const UPDATE_API = ['http://127.0.0.1:5002/api/server-cse-assistant-admin/activities']
+export const PREDICT_API = ['https://server-ner-prediction-second.herokuapp.com/api/tagging/v1'];
+export const LOGIN_API = ['https://admin-cse-assistant.herokuapp.com/auth']
+export const INSERT_API = ['https://admin-cse-assistant.herokuapp.com/api/server-cse-assistant-admin/activities']
+export const GET_DB_API = ['https://admin-cse-assistant.herokuapp.com/api/server-cse-assistant-admin/activities/page/']
+export const GET_DB_FILTER = ['https://admin-cse-assistant.herokuapp.com/api/server-cse-assistant-admin/activities/filter/page/']
+export const GET_SINGLE_API = ['https://admin-cse-assistant.herokuapp.com/api/server-cse-assistant-admin/activities/']
+export const DELETE_SINGLE_API = ['https://admin-cse-assistant.herokuapp.com/api/server-cse-assistant-admin/activities/']
+export const UPDATE_API = ['https://admin-cse-assistant.herokuapp.com/api/server-cse-assistant-admin/activities']
 
